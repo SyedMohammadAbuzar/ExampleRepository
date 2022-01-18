558e82215bef7e45b5005454d23d9402d956bda2
Fmajid29 <faizanmajid11@gmail.com>
2022-01-18 12:41:41 +0500

    alligned the question a lil better

--- 1.cpp
+++ 1.cpp
@@ -1,7 +1,10 @@
-/*Create a simple “shape” hierarchy: a base class called Shape and derived classes called Circle, Square, and Triangle.
+/*Create a simple “shape” hierarchy: 
+a base class called Shape and derived classes called Circle, Square, and Triangle.
 In the base class, make a virtual function called draw( ), and override this in the derived classes.
-Make an array of pointers to Shape objects that you create on the heap (and thus perform upcasting of the pointers), and
-call draw( ) through the base-class pointers, to verify the behavior of the virtual function. If your debugger supports it, single-step through the code*/
+Make an array of pointers to Shape objects that you create on the heap (and thus perform upcasting 
+of the pointers), and call draw( ) through the base-class pointers, 
+to verify the behavior of the virtual function. If your debugger supports it, 
+single-step through the code*/
 
 #include<iostream>
 using namespace std;
